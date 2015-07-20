79f5ca3a45ab62227837e2782f78ddcd9cf9671c
Ellery <383188743@qq.com>
2015-07-20 09:11:42 +0800

    Promise

--- XUtil/XUtil.js
+++ XUtil/XUtil.js
@@ -2572,12 +2572,15 @@ XUtil.paginator = function (opt) {
 
     var toPrev = function () {
 
-        toPage(--currentPage);
+        //为了记录前一页的index,这么做是必须的
+        var pageNum = currentPage - 1;
+        toPage(pageNum);
     };
 
     var toNext = function () {
 
-        toPage(++currentPage);
+        var pageNum = currentPage + 1;
+        toPage(pageNum);
     };
 
     var init = function () {
@@ -2896,6 +2899,128 @@ XUtil.XPopout = function (option) {
     return that;
 };
 
+/**
+ * Created by Ellery1 on 15/7/19.
+ */
+(function (global) {
+
+    function Promise() {
+
+        this.queue = [];
+        this.status = "pending";
+        this.isPromise = true;
+        this.parentPromise = null;
+    }
+
+    var fn = Promise.prototype;
+
+    fn._setFinalStatus = function (status) {
+
+        var rootPromise = this.parentPromise;
+
+        while (rootPromise) {
+
+            if (rootPromise.parentPromise) {
+
+                rootPromise = rootPromise.parentPromise;
+            }
+            else {
+
+                break;
+            }
+        }
+
+        this.status = status;
+        rootPromise && (rootPromise.status = status);
+
+        return rootPromise;
+    };
+
+    fn.then = function (done, fail) {
+
+        var status = this.status;
+
+        if (status === "pending") {
+
+            this.queue.push({
+                done: done,
+                fail: fail
+            });
+        }
+        else if (status === 'resolved') {
+
+            console.log('This promise has been resolved, done callback should be called immediately here.');
+            done && done.apply(global);
+        }
+        else if (status === 'rejected') {
+
+            console.log('This promise has been rejected, fail callback should be called immediately here.');
+            fail && fail.apply(global);
+        }
+
+        return this;
+    };
+
+    fn.resolve = function () {
+
+        var next,
+            nextDone,
+            args,
+            returned;
+
+        next = this.queue.shift();
+
+        if (next && this.status === 'pending') {
+
+            nextDone = next.done;
+            args = Array.prototype.slice.call(arguments).concat(this);
+            returned = nextDone.apply(global, args);
+
+            if (returned && returned.isPromise) {
+
+                returned.queue = this.queue;
+                returned.parentPromise = this;
+            }
+            else {
+
+                this.resolve(returned);
+            }
+        }
+        else if (!next) {
+
+            console.log('all callbacks called!');
+            this._setFinalStatus("resolved");
+        }
+        else {
+
+            console.log('This promise has been resolved/rejected!');
+        }
+    };
+
+    fn.reject = function () {
+
+        var next, nextFail, args, rootPromise;
+
+        next = this.queue.shift();
+
+        if (next) {
+
+            nextFail = next.fail;
+
+            if (nextFail) {
+
+                args = Array.prototype.slice.call(arguments).concat(this);
+                nextFail.apply(global, args);
+            }
+        }
+
+        this.queue = [];
+        rootPromise = this._setFinalStatus("rejected");
+        rootPromise && (rootPromise.queue = []);
+    };
+
+    XUtil.Promise = Promise;
+})(window);
 /*
  * 源文件：src/table.js
  * 表格widget，将一个JSON数组以表的形式展示，支持对表格数据进行增删改操作，以及分页，排序，选择，编辑，关键字过滤等多种功能
